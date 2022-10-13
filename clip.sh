ff7348733155eb2010821cca17eedad57cb6c452
Sibidine <87843731+Sibidine@users.noreply.github.com>
2022-10-13 15:09:05 +0530

    Add files via upload
    Apparently it was the pesky codec flag causing the issue- removing it solved the problem

--- clip.sh
+++ clip.sh
@@ -3,15 +3,12 @@
 #DECLARATIONS
 
 # USER INPUTS:
-# link
-# start_time
-# length
-# name_output
-# video_url
-# audio_url
-# video_length
-# download_type
-# file_type
+# link: takes the URL of the video to be downloaded
+# start_time: takes the starting point of the video from where to download
+# length: takes the length of the clip desired
+# name_output: takes the name of the output file to be made (without the extension)
+# video_length: takes a binary input for whether the full video is desired, or merely a portion of it
+# file_type: binary input for if audio or video is desired
 
 #INPUT
 
@@ -26,9 +23,11 @@ then
 	read file_type
 	if [[ file_type -eq 1 ]]
 	then
+		# AUDIO
 		youtube-dl -x --audio-format mp3 --prefer-ffmpeg "$link"
 	elif [[ file_type -eq 2 ]]
 	then
+		#  VIDEO
 		youtube-dl "$link"
 	fi
 
@@ -39,7 +38,19 @@ then
 	read -p "Enter length of clip: " length
 	read -p "Enter name of output file:" name_output 
 
+	echo "Enter 1 for audio, 2 for video"
+	read file_type
+
 	#GENERATING THE TRUE URLs FOR THE VIDEO
 	read -d'\n' video_url audio_url <<< $(youtube-dl --youtube-skip-dash-manifest -g "$link")
-	ffmpeg -ss $start_time -i "$video_url" -ss $start_time -i "$audio_url" -map 0:v -map 1:a -t $length -c:v libx264 -c:a aac $name_output.mp4
+
+	if [[ file_type -eq 1 ]]
+	then
+		# AUDIO
+		ffmpeg -ss $start_time -i "$audio_url" -t $length $name_output.mp3
+	elif [[ file_type -eq 2 ]]
+	then
+		# VIDEO	
+		ffmpeg -ss $start_time -i "$video_url" -ss $start_time -i "$audio_url" -map 0:v -map 1:a -t $length -c:v libx264 -c:a aac $name_output.mp4
+	fi
 fi
