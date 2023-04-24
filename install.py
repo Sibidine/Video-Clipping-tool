23b3814da520e708340062dd081f0e4de312f5cd
Rik <97382691+sontaimnt@users.noreply.github.com>
2023-04-24 22:20:30 +0530

    even more accesible

--- install.py
+++ install.py
@@ -6,24 +6,31 @@ from os import system, chdir
 from os.path import isfile
 
 # package managers(currently apt,pacman,dnf)
+print("checking package manager")
 path_apt = "/usr/bin/apt"
 path_pacman = "/usr/bin/pacman"
 path_dnf = "/usr/bin/dnf"
 
 # git is the only make depend
 if isfile(path_apt) == True:
+  print("apt found")
+  print("installing git , ffmpeg , yt-dlp")
   prcs = system("sudo apt install git ffmpeg yt-dlp")
   if prcs != 0:
     raise OSError("error: git failed to install")
   else:
     pass
 elif isfile(path_pacman) == True:
+  print("pacman found")
+  print("installing git , ffmpeg , yt-dlp")
   prcs = system("sudo pacman -S git ffmpeg yt-dlp")
   if prcs != 0:
     raise OSError("error: git failed to install")
   else:
     pass
-elif isfile(path_pacman) == True:
+elif isfile(path_dnf) == True:
+  print("dnf found")
+  print("installing git , ffmpeg , yt-dlp")
   prcs = system("sudo dnf install git ffmpeg yt-dlp")
   if prcs != 0:
     raise OSError("error: git failed to install")
@@ -40,6 +47,7 @@ if prcs != 0:
 else:
   pass
 
+print("changing directory")
 chdir("Video-Clipping-Tool")
 print("now installing Video-Clipping-Tool")
 prcs = system("sudo install -Dm755 clip.sh /usr/bin/clip")
