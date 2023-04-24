9163948d419be70c0750b08ad0df366addcdc458
Sibidine <87843731+Sibidine@users.noreply.github.com>
2023-04-24 22:23:22 +0530

    Update dependency_check.sh

--- dependency_check.sh
+++ dependency_check.sh
@@ -5,7 +5,7 @@ echo -n "Checking dependencies... "
 
 for dependency_name in yt-dlp ffmpeg
 do
-   [[ $(which $dependency_name 2>/dev/null) ]] || { echo -en "\n$dependency_name needs to be installed. Use 'sudo apt install $dependency_name'";deps=1; }
+   [[ $(which $dependency_name 2>/dev/null) ]] || { echo -en "\n$dependency_name needs to be installed.";deps=1; }
 done
 
 if [[ $deps -ne 1 ]]
